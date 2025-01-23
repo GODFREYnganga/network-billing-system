882daed8baf33353e39a7d890ba626f12edce41b
Godfrey Ng'ang'a <godfreynganga50@gmail.com>
2025-01-23 12:43:17 +0300

    API linking

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,14 @@ app.use('/api/routers', require('./routes/routers'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+require('dotenv').config();
+const { Pool } = require('pg');
+const africastalking = require('africastalking')({
+    apiKey: process.env.AFRICAS_TALKING_API_KEY,
+    });
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false } // Required for some cloud databases
+});
